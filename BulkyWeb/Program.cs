794fcec807247fbf64900cb488c5855b382fa1da
romaindago <dagostino.romain@hotmail.fr>
2023-07-30 19:32:59 +0200

    N TIers archi

--- BulkyWeb/Program.cs
+++ BulkyWeb/Program.cs
@@ -1,4 +1,4 @@
-using BulkyWeb.Data;
+using Bulky.DataAccess.Data;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -8,8 +8,7 @@ builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<ApplicationDbContext>(options => 
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-var app = builder.Build();
-
+var app = builder.Build(); 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
